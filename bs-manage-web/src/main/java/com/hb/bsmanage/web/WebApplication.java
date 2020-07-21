f668196ab94111247b6cd4136cbf56af04c6a046
黄彪 <191059014@qq.com>
2020-07-21 17:40:37 +0800

    创建项目

---
+++ bs-manage-web/src/main/java/com/hb/bsmanage/web/WebApplication.java
@@ -0,0 +1,27 @@
+package com.hb.bsmanage.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * 项目入口
+ */
+@SpringBootApplication
+public class WebApplication {
+    /**
+     * main方法
+     *
+     * @param args 启动参数
+     */
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(WebApplication.class, args);
+        ConfigurableEnvironment env = applicationContext.getEnvironment();
+        String host = env.getProperty("server.address");
+        String port = env.getProperty("server.port");
+        System.out.println("===========================================");
+        System.out.println(String.format(" http://%s:%s/index.html", host == null ? "localhost" : host, port));
+        System.out.println("===========================================");
+    }
+}
