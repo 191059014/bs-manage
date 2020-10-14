47562fb6eaeaa2737d17350593fb999f2c61c29f
黄彪 <191059014@qq.com>
2020-10-14 16:16:51 +0800

    优化代码

--- bs-manage-web/src/main/java/com/hb/bsmanage/web/security/service/RbacAuthorityService.java
+++ bs-manage-web/src/main/java/com/hb/bsmanage/web/security/service/RbacAuthorityService.java
@@ -1,12 +1,22 @@
 package com.hb.bsmanage.web.security.service;
 
 import com.hb.bsmanage.web.WebApplication;
+import com.hb.bsmanage.web.common.enums.ResourceType;
+import com.hb.bsmanage.web.dao.po.SysPermissionPO;
+import com.hb.bsmanage.web.security.util.SecurityUtils;
+import com.hb.unic.base.util.LogHelper;
+import com.hb.unic.logger.Logger;
+import com.hb.unic.logger.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * url动态权限验证
@@ -14,9 +24,14 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class RbacAuthorityService {
 
-    @Autowired
-    private RequestMappingHandlerMapping mapping;
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RbacAuthorityService.class);
 
+    /**
+     * HttpServletRequest
+     */
     @Autowired
     private HttpServletRequest request;
 
@@ -27,40 +42,24 @@ public class RbacAuthorityService {
      * @return true为有权限
      */
     public boolean hasPermission(Authentication authentication) {
-        if (WebApplication.testModeIsOpen()) {
-            System.out.println("RbacAuthorityService.hasPermission（模拟）");
-            return false;
-        }
-        Object userInfo = authentication.getPrincipal();
+        String baseLog = LogHelper.getBaseLog("动态权限验证");
         boolean hasPermission = false;
-
-//        if (userInfo instanceof UserDetails) {
-//            UserPrincipal principal = (UserPrincipal) userInfo;
-//            String userId = principal.getUser().getUserId();
-//
-//            List<Role> roles = this.selectByUserId(userId);
-//            List<String> roleIds = roles.stream().map(Role::getRoleId).collect(Collectors.toList());
-//            List<Permission> permissions = this.selectByRoleIdList(roleIds);
-//
-//            //获取资源，前后端分离，所以过滤页面权限，只保留按钮权限
-//            List<Permission> btnPerms = permissions.stream()
-//                    // 过滤页面权限
-//                    .filter(permission -> Objects.equals(permission.getType(), Consts.BUTTON))
-//                    // 过滤 URL 为空
-//                    .filter(permission -> !StrUtils.isEmpty(permission.getUrl()))
-//                    // 过滤 METHOD 为空
-//                    .filter(permission -> !StrUtils.isEmpty(permission.getMethod()))
-//                    .collect(Collectors.toList());
-//
-//            for (Permission btnPerm : btnPerms) {
-//                AntPathRequestMatcher antPathMatcher = new AntPathRequestMatcher(btnPerm.getUrl(), btnPerm.getMethod());// 也可以只通过url来匹配
-//                if (antPathMatcher.matches(request)) {
-//                    hasPermission = true;
-//                    break;
-//                }
-//            }
-//        }
-        System.out.println("RbacAuthorityService.hasPermission: " + hasPermission);
+        if (WebApplication.testModeIsOpen()) {
+            hasPermission = true;
+        } else {
+            List<SysPermissionPO> sysPermissionList = SecurityUtils.getCurrentUserPermissions();
+            Set<String> allPermissions = sysPermissionList.stream().map(SysPermissionPO::getValue).collect(Collectors.toSet());
+            //获取资源，前后端分离，所以过滤页面权限，只保留按钮权限
+            Set<String> buttonPermissions = allPermissions.stream().filter(p -> ResourceType.BUTTON.getValue().equals(p)).collect(Collectors.toSet());
+            for (String btnPerm : buttonPermissions) {
+                AntPathRequestMatcher antPathMatcher = new AntPathRequestMatcher(btnPerm);// 也可以只通过url来匹配
+                if (antPathMatcher.matches(request)) {
+                    hasPermission = true;
+                    break;
+                }
+            }
+        }
+        LOGGER.info("{}结束={}", baseLog, hasPermission);
         return hasPermission;
     }
 
